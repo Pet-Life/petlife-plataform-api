7b3d61a96ff90b27efb31c882973e5e0d383943b
DouglasPimentel <dev.pimentel@gmail.com>
2020-04-09 09:52:53 -0300

    Refatorado UserController

--- src/controllers/UserController.js
+++ src/controllers/UserController.js
@@ -4,17 +4,28 @@ class UserController {
   static async create(req, res) {
     const { firstName, lastName, email, password, permissionLevel } = req.body;
 
-    const user = await User.create({
+    await User.create({
       firstName,
       lastName,
       email,
       password,
       permissionLevel,
-    });
+    })
+      .then((user) => {
+        user.password = undefined;
 
-    return res
-      .status(201)
-      .json({ success: true, message: 'created new user', user });
+        return res
+          .status(201)
+          .json({ success: true, message: 'created new user', user });
+      })
+      .catch((err) => {
+        res.status(200).json({
+          success: false,
+          message: 'error create new user',
+          error: 'email already register',
+        });
+        console.log(err);
+      });
   }
 }
 

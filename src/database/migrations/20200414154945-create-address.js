d6e2c3ae33f6f99fb1b8b0e1cfce89efb284f298
DouglasPimentel <dev.pimentel@gmail.com>
2020-04-14 13:09:42 -0300

    Create 20200414154945-create-address.js

---
+++ src/database/migrations/20200414154945-create-address.js
@@ -0,0 +1,56 @@
+module.exports = {
+  up: (queryInterface, Sequelize) => {
+    return queryInterface.createTable('adresses', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER,
+      },
+      zipcode: {
+        allowNull: false,
+        type: Sequelize.INTEGER,
+      },
+      address: {
+        allowNull: false,
+        type: Sequelize.STRING,
+      },
+      number: {
+        type: Sequelize.INTEGER,
+      },
+      complement: {
+        type: Sequelize.STRING,
+      },
+      city: {
+        allowNull: false,
+        type: Sequelize.STRING,
+      },
+      state: {
+        allowNull: false,
+        type: Sequelize.STRING,
+      },
+      coordinates: {
+        type: Sequelize.ARRAY(Sequelize.REAL),
+      },
+      consumerId: {
+        references: {
+          model: 'consumer',
+          key: 'id',
+        },
+        type: Sequelize.INTEGER,
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE,
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE,
+      },
+    });
+  },
+
+  down: (queryInterface) => {
+    return queryInterface.dropTable('adresses');
+  },
+};

c3b3965f69f65a727ab3478cd5b0f404bf156254
DouglasPimentel <dev.pimentel@gmail.com>
2020-04-25 09:18:34 -0300

    criado funcionalidade para tratar connections com socket.io

--- src/server.js
+++ src/server.js
@@ -2,11 +2,15 @@ const http = require('http');
 const dotenv = require('dotenv');
 const app = require('./app');
 
+const { setupWebsocket } = require('./websocket');
+
 dotenv.config();
 
 const API_PORT = process.env.API_PORT || 3333;
 
 const server = http.createServer(app);
+
+setupWebsocket(server);
 
 server.listen(API_PORT, () => {
   console.log(`Server running on the http://127.0.0.1:${API_PORT}/api/v1/`);

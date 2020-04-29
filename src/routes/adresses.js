ff50831c6d53ca6e62eaccd72fd693f511be6b2b
DouglasPimentel <dev.pimentel@gmail.com>
2020-04-29 10:17:52 -0300

    alterado forma de busca de endereço

--- src/routes/adresses.js
+++ src/routes/adresses.js
@@ -6,6 +6,6 @@ const router = express.Router();
 router.get('/consumers/:id/adresses', AddressController.getById);
 router.post('/consumers/:id/adresses', AddressController.create);
 
-router.get('/adresses', AddressController.findAddress);
+router.post('/adresses', AddressController.findAddress);
 
 module.exports = router;
